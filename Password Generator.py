97a4194481f9ad6438b1d2537ce588d8874a0699
Mykola Antoniv <mykolaantoniv@gmail.com>
2023-10-30 12:04:38 +0200

    made refactoring

--- Password Generator.py
+++ Password Generator.py
@@ -9,11 +9,8 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-random_letters = []
 random_letters = random.choices(letters, k=nr_letters)
-random_symbols = []
 random_symbols = random.choices(symbols, k=nr_symbols)
-random_numbers = []
 random_numbers = random.choices(numbers, k=nr_numbers)
 
 result_list = random_letters + random_symbols + random_numbers
